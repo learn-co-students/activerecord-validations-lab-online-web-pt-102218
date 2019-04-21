5f11dec9f9f0e71238450e69a18e9d8b8bd650bd
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 01:30:09 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presense: true 
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(fiction non-fiction) }
+  validates :title, exclusion: { in: %w(/\b(Won't Believe|Secret|Top \d|Guess)\b/) }
 end
