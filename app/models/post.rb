50dcaafb7064a43fd76c2818628fe2b796802dba
beaulieu527 <beaulieudev@gmail.com>
2019-04-24 20:39:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
-class Post < ActiveRecord::Base
+class MyValidator < ActiveModel::Validator
+    def validate(post)
+        if post.title
+            post.errors[:title] << 'false' if ["Won't Believe", "Secret", "Top [number]", "Guess"].none? do |phrase|
+             post.title.include?(phrase)
+            end
+        end
+    end
 end
+
+class Post < ActiveRecord::Base
+    validates_with MyValidator
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+end
+
+
